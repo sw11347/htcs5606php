58e463c814d530ed70a73a9ee46ebe5292050b9a
Simon Woods <sw11347@gmail.com>
2020-05-18 10:15:12 +1200

    created while loop

--- week5/class1.php
+++ week5/class1.php
@@ -5,3 +5,9 @@
     echo "<p style='font-family: Bahnschrift'>Hello in Paragraph with style.</p>";
 
 //https://htcs5606php.herokuapp.com/
+
+$i = 0; //set counter
+while ($i < 100){ //set condition
+    echo "<p>Hello</p>"; //set action if condition true
+    $i = $i + 1; //update counter
+}
