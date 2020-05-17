9c240b38d3eb34315213651376a602495e264539
Simon Woods <sw11347@gmail.com>
2020-05-18 09:35:20 +1200

    class 1 for echo elements

---
+++ Week5/class1.php
@@ -0,0 +1,5 @@
+<?php
+    echo "hello world";
+    echo "<h1>Hello in Heading</h1>";
+    echo "<p>Hello in Paragraph</p>";
+    echo "<p style='font-family: Bahnschrift'>Hello in Paragraph with style.</p>";
