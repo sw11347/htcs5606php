becaa4ce5c21adb83dff528f0db211beca1fd0bd
Simon Woods <sw11347@gmail.com>
2020-05-18 10:39:02 +1200

    html+php

---
+++ week5/class1_2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$i = 0;
+while ($i < 100) {
+    echo "<p>Hello</p>";
+    $i = $i + 1;
+}
+?>
+</body>
+</html>
