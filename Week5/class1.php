7c969a5e5c52ad061d528ee56d88d83e310ed57e
Simon Woods <sw11347@gmail.com>
2020-05-18 10:00:43 +1200

    class 1

--- Week5/class1.php
+++ Week5/class1.php
@@ -3,3 +3,5 @@
     echo "<h1>Hello in Heading</h1>";
     echo "<p>Hello in Paragraph</p>";
     echo "<p style='font-family: Bahnschrift'>Hello in Paragraph with style.</p>";
+
+//https://htcs5606php.herokuapp.com/
