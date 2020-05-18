06d068f0ad821233cfee9c3245d61bc31853a600
Simon Woods <sw11347@gmail.com>
2020-05-19 09:07:23 +1200

    number update

--- week5/class2.php
+++ week5/class2.php
@@ -9,6 +9,6 @@
     echo "<h1>".$greeting." ".$who."</h1>"; //dot means merge strings.
 $x = 4;
 $y = 5;
-echo "x =".$x;
-echo "y =".$y;
-echo "z = x + y".($x + $y);
+echo " x = ".$x;
+echo " y = ".$y;
+echo " z = x + y = ".($x + $y);
