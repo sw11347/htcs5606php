45fe7ecea48dd0443f04f8dc89f8d8f38b116f30
Simon Woods <sw11347@gmail.com>
2020-05-19 09:36:13 +1200

    string methods

--- week5/class2.php
+++ week5/class2.php
@@ -12,3 +12,18 @@ $y = 5;
 echo " x = ".$x;
 echo " y = ".$y;
 echo " z = x + y = ".($x + $y);
+
+
+$greeting = "Hello World";
+//length of string
+echo "<br>".strlen($greeting)."<br>";
+// word count -- count number of words in that string
+echo str_word_count($greeting)."<br>";
+// string reverse
+echo strrev($greeting)."<br>";
+// string word position
+echo strpos($greeting,"hello")."word position<br>";
+//string replace
+echo str_replace($greeting, "hello", "bye bye")."word replace<br>";
+//to uppercase
+echo strtoupper($greeting)."To Uppercase<br>";
