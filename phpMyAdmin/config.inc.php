a0ab3142c5596c2fd67e0da56adc1b65af63719c
Simon Woods <sw11347@gmail.com>
2020-05-21 09:50:40 +1200

    my database config

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -28,10 +28,10 @@ $i++;
 /* Authentication type */
 //mysql://sf6mcpjg9mp3fqnc:zv91vgnxsb4bggba@kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hwedspsyvc8ffi0y
 $cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['user'] = 'sf6mcpjg9mp3fqnc';
-$cfg['Servers'][$i]['password'] = 'zv91vgnxsb4bggba';
+$cfg['Servers'][$i]['user'] = 'tm99sruc6jy9h74t';
+$cfg['Servers'][$i]['password'] = 'hhu7pv1bfy5lbr6b';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$cfg['Servers'][$i]['host'] = 'tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
