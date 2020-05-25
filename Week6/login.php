99a0a3a70b7782eb37a9a5a4a5682160c6f2e211
Simon Woods <sw11347@gmail.com>
2020-05-26 10:23:41 +1200

    update

--- Week6/login.php
+++ Week6/login.php
@@ -15,7 +15,7 @@ if (isset($_POST["username"])) {
     $server = "tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $user = "tm99sruc6jy9h74t";
     $pwd = "hhu7pv1bfy5lbr6b";
-    $database = "eq8apmg97080j54s";
+    $database = "binwf1dyl1cixxwb";
 
     $connection = new mysqli($server, $user, $pwd, $database); //create database connection
     if ($connection->connect_error) {
