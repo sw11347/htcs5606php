14b92c1b23ad0eef443199b0bb3f8d5560711a50
Simon Woods <sw11347@gmail.com>
2020-05-28 09:24:25 +1200

    update - Login.php

---
+++ Week6/loginform.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>loginform</title>
+</head>
+<body>
+
+<?php
+include_once "login.php";
+session_start();
+if (isset($_SESSION["username"])){
+    ?>
+    <p><a href="profile.php"></a>Profile</p>
+    <p><a href="changepassword.php"></a>Change Password</p>
+    <p><a href="logout.php"></a>Logout</p>
+    <?php
+} else{
+    ?>
+    <form action="login.php" method="post">
+    <p>Username <input type="text" name="username" required></p>
+    <p>Password <input type="password" name="pwd" required></p>
+    <p><input type="submit" value="login"></p>
+</form>
+<?php
+}
+?>
+
+</body>
+</html>
