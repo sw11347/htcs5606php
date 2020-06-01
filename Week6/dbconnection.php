46e303c10339977c78524db57c2d3691194cd4fc
Simon Woods <sw11347@gmail.com>
2020-06-02 09:44:38 +1200

    update

--- Week6/dbconnection.php
+++ Week6/dbconnection.php
@@ -7,8 +7,9 @@ $database = "binwf1dyl1cixxwb";
 /**
  * @return mysqli
  */
-function dbconn(){
-    $connection = new mysqli($GLOBALS['server'],$GLOBALS['user'], $GLOBALS['pwd'],$GLOBALS['database']); //create database connection
+function dbconn()
+{
+    $connection = new mysqli($GLOBALS['server'], $GLOBALS['user'], $GLOBALS['pwd'], $GLOBALS['database']); //create database connection
     if ($connection->connect_error) {
         echo $connection->connect_error;
     } else {
@@ -17,18 +18,19 @@ function dbconn(){
 }
 
 
-
-function login($username, $password){
+function login($username, $password)
+{
     //return true/false gives result back and jumps out of function. Any code after return ignored.
- $connection = dbconn();
+    $connection = dbconn();
 
     $sql = "select * from Users where username = '$username'"; //This is our query
+   echo $sql;
     $result = $connection->query($sql); //run query on this connection through method query()
     if ($result->num_rows == 1) { // means user exists in database
         while ($row = $result->fetch_assoc()) {
             if ($row["password"] == $password) {
-            $connection->close();
-            return true;
+                $connection->close();
+                return true;
             } else {
                 $connection->close();
                 return false;
