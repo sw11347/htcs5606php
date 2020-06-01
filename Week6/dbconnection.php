4be13b16099ae379cab0a1048838f64b7a1a7e38
Simon Woods <sw11347@gmail.com>
2020-06-02 09:31:43 +1200

    login function

--- Week6/dbconnection.php
+++ Week6/dbconnection.php
@@ -4,9 +4,41 @@ $user = "tm99sruc6jy9h74t";
 $pwd = "hhu7pv1bfy5lbr6b";
 $database = "binwf1dyl1cixxwb";
 
-$connection = new mysqli($server, $user, $pwd, $database); //create database connection
-if ($connection->connect_error) {
-    echo $connection->connect_error;
-} else {
-    echo "Connection Created";
+/**
+ * @return mysqli
+ */
+function dbconn(){
+    $connection = new mysqli($GLOBALS['server'],$GLOBALS['user'], $GLOBALS['pwd'],$GLOBALS['database']); //create database connection
+    if ($connection->connect_error) {
+        echo $connection->connect_error;
+    } else {
+        return $connection;
+    }
+}
+
+
+
+function login($username, $password){
+    //return true/false gives result back and jumps out of function. Any code after return ignored.
+ $connection = dbconn();
+
+    $sql = "select * from Users where username = '$username'"; //This is our query
+    $result = $connection->query($sql); //run query on this connection through method query()
+    if ($result->num_rows == 1) { // means user exists in database
+        while ($row = $result->fetch_assoc()) {
+            if ($row["password"] == $password) {
+            $connection->close();
+            return true;
+            } else {
+                $connection->close();
+                return false;
+            }
+        }
+
+    } else {
+
+        $connection->close();
+        return false;
+    }
+
 }
--- Week6/login.php
+++ Week6/login.php
@@ -7,48 +7,18 @@
  */
 
 if (isset($_POST["username"])) {
-
-
     $username = $_POST["username"];
-    $password = $_POST["passwordd"];
+    $password = $_POST["password"];
 
     include_once "dbconnection.php";
-
-    $sql = "select * from Users where username = '$username'"; //This is our query
-    $result = $connection->query($sql); //run query on this connection through method query()
-    if ($result->num_rows == 1) { // means user exists in database
-        while ($row = $result->fetch_assoc()) {
-            if ($row["password"] == $password) {
-                echo "access granted";
-                session_start();
-                $_SESSION["username"] = $username;
-            } else {
-                echo "wrong password";
-                ?>
-                <script>
-                    setTimeout(function () {
-                        window.open("loginform.php.", "_self");
-                    }, 3000);
-                </script>
-                <?php
-            }
-        }
-
-    } else {
-        echo "wrong username";
-        ?>
-        <script>
-            setTimeout(function () {
-                window.open("loginform.php.", "_self");
-            }, 3000);
-        </script>
-        <?php
-
+//    I will use my login function
+    if(login($username,$password)){
+        session_start();
+        $_SESSION["username"] = $username;
     }
-    $connection->close(); // close my connection
-}
-else {
+}else {
 
-
+    echo "Wrong username or password";
     }
-    ?>
+
+?>
