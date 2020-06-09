c96846219071689d288a92c6652b9d1538b31589
Simon Woods <sw11347@gmail.com>
2020-06-10 08:53:59 +1200

    view category nam

--- HTCS5606_Casestudy1/index.php
+++ HTCS5606_Casestudy1/index.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <title>Simon Assessment 1 Home page</title>
     <link rel="stylesheet" href="CSS/index_style.css">
+    <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
+
     <script src="JS/pet_foods_script.js"></script>
 </head>
 <body onload="move_logo">
