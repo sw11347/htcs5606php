aa94d834f6fa7a38dded1eef26304e849dd34ab7
Simon Woods <sw11347@gmail.com>
2020-06-10 09:43:03 +1200

    add to cart

--- HTCS5606_Casestudy1/index.php
+++ HTCS5606_Casestudy1/index.php
@@ -8,7 +8,7 @@
         src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
-
+  <script src="JS/pet_foods_script.js"></script>
 
 </head>
 <body onload="move_logo">
