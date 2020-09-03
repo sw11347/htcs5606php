6a9315dc4daa6aea78bd7d092455db3e4b697398
Simon Woods <sw11347@gmail.com>
2020-09-04 10:33:34 +1200

    update

--- HTCS5606_Casestudy1/index.php
+++ HTCS5606_Casestudy1/index.php
@@ -5,10 +5,10 @@
     <title>Simon Assessment 1 Home page</title>
     <link rel="stylesheet" href="CSS/index_style.css">
     <script
-        src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-        crossorigin="anonymous"></script>
-  <script src="JS/pet_foods_script.js"></script>
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
+    <script src="JS/pet_foods_script.js"></script>
 
 </head>
 <body onload="move_logo">
@@ -18,19 +18,19 @@ include_once "header.php";
 ?>
 
 <div id="main">
-    <table id = "main_table">
+    <table id="main_table">
         <tr>
             <td id="middle_content">
-    <h2>Welcome to Pet Foods Online Store!</h2>
-            <h2 id="index_product_heading">Popular purchases</h2>
-<!--                Below are all the products and prices-->
+                <h2>Welcome to Pet Foods Online Store!</h2>
+                <h2 id="index_product_heading">Popular purchases</h2>
+                <!--                Below are all the products and prices-->
                 <?php
 
 
                 $products = $user->show_all_products();
 
                 $i = 0;
-                while ($i<sizeof($products)){
+                while ($i < sizeof($products)) {
                     $product = $products[$i];
                     ?>
 
@@ -41,14 +41,15 @@ include_once "header.php";
                         <br>
                         <button name="<?php echo $product->name; ?>"
                                 price="<?php echo $product->price; ?>"
-                                onclick="add_to_cart(this)">Add to cart</button>
+                                onclick="add_to_cart(this)">Add to cart
+                        </button>
                     </div>
                     <?php
                     $i = $i + 1;
                 }
                 ?>
             </td>
-<!--            Add to cart section of the page-->
+            <!--            Add to cart section of the page-->
             <td id="right_content">
                 <p>Cart</p>
                 <div id="cart_div"></div>
@@ -57,14 +58,16 @@ include_once "header.php";
             </td>
         </tr>
     </table>
+
 </div>
 
-    <div id="myModal" class="modal">
+<div id="myModal" class="modal">
     <!-- Modal content -->
     <div class="modal-content">
         <span class="close">&times;</span>
-        <div id = "invoicePlace">Invoice</div>
+        <div id="invoicePlace">Invoice</div>
     </div>
+
 
 </div>
 
